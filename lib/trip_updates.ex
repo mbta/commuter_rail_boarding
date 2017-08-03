986a557205dfde515e9d9c52049ba0263a4bd48f
Paul Swartz <paulswartz@gmail.com>
2017-08-03 10:45:47 -0400

    add tests for TripUpdate builder

--- lib/trip_updates.ex
+++ lib/trip_updates.ex
@@ -1,6 +1,10 @@
 defmodule TripUpdates do
   @moduledoc """
   Responsible for converting lists of BoardingStatus structs into an enhanced TripUpdates JSON feed
+
+  The basic TripUpdates feed is a Protobuf, documented here: https://developers.google.com/transit/gtfs-realtime/guides/trip-updates
+
+  The enhanced JSON feed takes the Protobuf, expands it into JSON, and adds some additional fields.
   """
 
   def to_map(boarding_statuses) do
@@ -19,33 +23,60 @@ defmodule TripUpdates do
   end
 
   def entity(current_time, boarding_statuses) do
-    for {trip_id, trip_statuses} <- Enum.group_by(boarding_statuses, & &1.trip_id) do
-      status = List.first(trip_statuses)
-      %{
-        id: "#{current_time}_#{trip_id}",
-        trip_update: %{
-          trip: %{
-            trip_id: trip_id,
-            route_id: status.route_id,
-            direction_id: status.direction_id,
-            start_date: DateTime.to_date(status.scheduled_time),
-            schedule_relationship: "SCHEDULED"
-          },
-          stop_time_update: Enum.map(trip_statuses, &stop_time_update/1)
-        }
-      }
+    for {_trip_id, trip_statuses} <- Enum.group_by(
+          boarding_statuses, & &1.trip_id),
+      update <- trip_update(current_time, trip_statuses) do
+        update
     end
   end
 
-  def stop_time_update(boarding_status) do
+  def trip_update(_current_time, []) do
+    []
+  end
+  def trip_update(current_time, [%BoardingStatus{} = status | _] = statuses) do
+    [
+      %{
+        id: "#{current_time}_#{status.trip_id}",
+        trip_update: %{
+          trip: trip(status),
+          stop_time_update: Enum.map(statuses, &stop_time_update/1)
+        }
+      }
+    ]
+  end
+
+  def trip(%BoardingStatus{} = status) do
+    start_date = case status.scheduled_time do
+                   :unknown -> Date.utc_today()
+                   dt -> DateTime.to_date(dt)
+                 end
     %{
-      stop_id: boarding_status.stop_id,
-      departure: %{
-        time: boarding_status.predicted_time,
+      trip_id: status.trip_id,
+      route_id: status.route_id,
+      direction_id: status.direction_id,
+      start_date: start_date,
+      schedule_relationship: "SCHEDULED"
+    }
+  end
+
+  def stop_time_update(%BoardingStatus{} = status) do
+    Map.merge(
+      %{
+        stop_id: status.stop_id,
+        boarding_status: status.boarding_status,
+        platform_id: status.track
       },
-      schedule_relationship: "SCHEDULED",
-      boarding_status: boarding_status.boarding_status,
-      platform_id: boarding_status.track
+      departure_map(status.predicted_time))
+  end
+
+  defp departure_map(:unknown) do
+    %{}
+  end
+  defp departure_map(%DateTime{} = dt) do
+    %{
+      departure: %{
+        time: DateTime.to_unix(dt)
+      }
     }
   end
 end
---
+++ test/trip_updates_test.exs
@@ -0,0 +1,107 @@
+defmodule TripUpdatesTest do
+  @moduledoc false
+  use ExUnit.Case, async: true
+
+  import TripUpdates
+
+  describe "to_map/1" do
+    test "returns the version in the header" do
+      map = to_map([])
+      assert map.header.gtfs_realtime_version == "1.0"
+    end
+
+    test "returns a current timestamp in the header" do
+      before_dt = utc_now()
+      map = to_map([])
+      header_dt = DateTime.from_unix!(map.header.timestamp)
+      after_dt = utc_now()
+
+      # they can be equal, but header_dt should be between before_dt and
+      # after_dt
+      refute DateTime.compare(before_dt, header_dt) == :gt
+      refute DateTime.compare(after_dt, header_dt) == :lt
+    end
+
+    test "returns a list of trip update entities" do
+      map = to_map([%BoardingStatus{}, %BoardingStatus{}])
+      assert [_] = map.entity
+    end
+  end
+
+  describe "entity/2" do
+    test "groups statuses by their trip_id" do
+      statuses = for trip_id <- ["1", "2", "1"] do
+        %BoardingStatus{trip_id: trip_id}
+      end
+      entities = entity(1234, statuses)
+      assert [one, two] = entities
+      assert [_, _] = one.trip_update.stop_time_update
+      assert [_] = two.trip_update.stop_time_update
+    end
+
+    test "returns an empty list of there are no statuses" do
+      assert entity(1234, []) == []
+    end
+  end
+
+  describe "trip_update/2" do
+    test "returns an empty list if there are no updates" do
+      assert trip_update(1234, []) == []
+    end
+
+    test "uses the trip_id from the first status and the time for the ID" do
+      status = %BoardingStatus{trip_id: "trip_id"}
+      assert [update] = trip_update(1234, [status, %BoardingStatus{}])
+      assert update.id == "1234_trip_id"
+    end
+
+    test "builds a trip_update for the statuses" do
+      assert [update] = trip_update(
+        1234, [%BoardingStatus{}, %BoardingStatus{}])
+      assert %{} = update.trip_update.trip
+      assert [%{}, %{}] = update.trip_update.stop_time_update
+    end
+  end
+
+  describe "trip/1" do
+    test "builds trip information from the status" do
+      status = %BoardingStatus{
+        scheduled_time: DateTime.from_naive!(~N[2017-02-05T05:06:07], "Etc/UTC"),
+        route_id: "route",
+        trip_id: "trip",
+        direction_id: 1
+      }
+      assert trip(status) == %{
+        trip_id: "trip",
+        route_id: "route",
+        direction_id: 1,
+        start_date: ~D[2017-02-05],
+        schedule_relationship: "SCHEDULED"
+      }
+    end
+  end
+
+  describe "stop_time_update/1" do
+    test "builds stop time update from the status" do
+      status = %BoardingStatus{
+        predicted_time: DateTime.from_unix!(12_345),
+        stop_id: "stop",
+        boarding_status: "status",
+        track: "track"
+      }
+      assert stop_time_update(status) == %{
+        stop_id: "stop",
+        departure: %{
+          time: 12_345
+        },
+        boarding_status: "status",
+        platform_id: "track"
+      }
+    end
+  end
+
+  defp utc_now do
+    # returns a DateTime in UTC, but without the microseconds
+    %{DateTime.utc_now | microsecond: {0, 0}}
+  end
+end
