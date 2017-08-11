67e6e2f0cb0049d5578b9fd3e3806e845f80a6d6
Chris Hayes <chayes@mbta.com>
2017-08-11 16:51:13 -0400

    Parsing entire files
    Slight refactoring of parsing, and adding functionality to parse all data out of each line, as well as parsing an entire file at once.

--- lib/parser.ex
+++ lib/parser.ex
@@ -1,13 +1,89 @@
 defmodule Parser do
-  @moduledoc """
-  Parser for PTIS data
-  """
+    @moduledoc """
+    Parser for PTIS data
+    """
 
-  def parse_line(line) do
-    split_line = String.split(line, " - ")
-    split_prefix = String.split_at(Enum.at(split_line,0), 22)
-    {:ok, timestamp} = Timex.parse(elem(split_prefix, 0), "{0M}-{0D}-{YYYY} {0h12}:{0m}:{0s} {AM}")
-    vehicle_id = elem(String.split_at(elem(split_prefix, 1), 12), 1)
-    {timestamp, vehicle_id, Enum.at(split_line, 1)}
-  end
+    def parse_line(line) do
+        split_line = String.split(line, " - ")
+        line_prefix = Enum.at(split_line,0)
+        if String.length(line_prefix) == 38 do
+            split_prefix = String.split_at(line_prefix, 22)
+            timestamp = case Timex.parse(elem(split_prefix, 0), "{0M}-{0D}-{YYYY} {0h12}:{0m}:{0s} {AM}") do
+                {:ok, result} -> result
+                {:error, reason} -> {:error, reason}
+            end
+            if timestamp == {:error, :eshortline} do
+                {:error, :eshortline}
+            else
+                vehicle_id = elem(String.split_at(elem(split_prefix, 1), 12), 1)
+                case parse_msg_body(Enum.at(split_line, 1)) do
+                    {:ok, parsed_body} -> {:ok, Map.merge(%{timestamp: timestamp, vehicle_id: vehicle_id}, parsed_body)}
+                    {:error, reason} -> {:error, reason}
+                end
+            end
+        else
+            {:error, :eshortline}
+        end
+    end
+
+    def parse_msg_body(data) do
+        [type, body] = String.split(data, "[")
+        data_map = %{type: type}
+        if Map.get(data_map, :type) == "Location" do
+            split_body = String.split(elem(String.split_at(body, -1), 0), ", ")
+            if length(split_body) == 4 do
+                [operator_clause, workpiece_clause, pattern_clause, gps_clause] = split_body
+                gps_parse_result = parse_gps(Enum.at(String.split(gps_clause, ":"), 1))
+                parsed_gps = case gps_parse_result do
+                    {:error, reason} -> {:error, reason}
+                    {:ok, result} -> result
+                end
+                if parsed_gps == {:error, :eshortline} do
+                    parsed_gps
+                else
+                    {:ok, Map.merge(data_map, %{operator: split_by_colon(operator_clause), workpiece: split_by_colon(workpiece_clause), pattern: split_by_colon(pattern_clause), gps: parsed_gps})}
+                end
+            else
+                {:error, :eshortline}
+            end
+        else
+            {:ok, data_map}
+        end
+    end
+
+    def split_by_colon(input) do
+        Enum.at(String.split(input, ":"), 1)
+    end
+
+    def parse_gps(gps_data) do
+        if String.starts_with?(gps_data, ">RPV") and String.length(gps_data) >= 35 do
+            {_, rest} = String.split_at(gps_data, 4)
+            {time, rest} = String.split_at(rest, 5)
+            {lat, rest} = String.split_at(rest, 8)
+            {long, rest} = String.split_at(rest, 9)
+            {speed, rest} = String.split_at(rest, 3)
+            {heading, rest} = String.split_at(rest, 3)
+            {source, rest} = String.split_at(rest, 1)
+            {age, _} = String.split_at(rest, 1)
+            {:ok, %{time: time, lat: lat, long: long, speed: speed, heading: heading, source: source, age: age}}
+        else
+            {:error, :eshortline}
+        end
+    end
+
+    def parse_file(file_path) do
+        {:ok, file} = File.open(file_path, [:read])
+        read_lines(file, IO.read(file, :line), [])
+    end
+
+    def read_lines(file, :eof, data) do
+        data
+    end
+
+    def read_lines(file, this_line, data) do
+        case parse_line(this_line) do
+            {:ok, parsed_line} -> read_lines(file, IO.read(file, :line), [parsed_line | data])
+            {:error, :eshortline} -> data
+        end
+    end
 end
--- test/parser_test.exs
+++ test/parser_test.exs
@@ -1,9 +1,87 @@
 defmodule ParserTest do
-  use ExUnit.Case
-  doctest Parser
+    use ExUnit.Case
+    doctest Parser
 
-  test "parses line from PTIS" do
-    assert Parser.parse_line("08-04-2017 11:01:51 AM	Vehicle ID:1712 - Location[Operator:910, Workpiece:802, Pattern:509, GPS:>RPV54109+4224023-0711289000018812<]") ==
-        {~N[2017-08-04 11:01:51], "1712", "Location[Operator:910, Workpiece:802, Pattern:509, GPS:>RPV54109+4224023-0711289000018812<]"}
-  end
+    test "parses line from PTIS" do
+        good_result = Parser.parse_line("08-04-2017 11:01:51 AM	Vehicle ID:1712 - Location[Operator:910, Workpiece:802, Pattern:509, GPS:>RPV54109+4224023-0711289000018812<]")
+        assert good_result == {:ok,
+            %{
+                timestamp: ~N[2017-08-04 11:01:51],
+                vehicle_id: "1712",
+                type: "Location",
+                operator: "910",
+                workpiece: "802",
+                pattern: "509",
+                gps: %{
+                    time: "54109",
+                    lat: "+4224023",
+                    long: "-07112890",
+                    speed: "000",
+                    heading: "188",
+                    source: "1",
+                    age: "2"
+                }
+            }}
+        #line truncated within timestamp
+        bad_result_one = Parser.parse_line("08-0")
+        assert bad_result_one == {:error, :eshortline}
+
+        #line truncated within Vehicle ID clause
+        bad_result_two = Parser.parse_line("08-04-2017 11:01:51 AM	Vehic")
+        assert bad_result_two == {:error, :eshortline}
+
+        #line truncated arbitrarily within Location object
+        bad_result_three = Parser.parse_line("08-04-2017 11:01:51 AM	Vehicle ID:1712 - Location[Operator:910, Workpie")
+        assert bad_result_three == {:error, :eshortline}
+
+        #line truncated within GPS data
+        bad_result_four = Parser.parse_line("08-04-2017 11:01:51 AM	Vehicle ID:1712 - Location[Operator:910, Workpiece:802, Pattern:509, GPS:>RPV54109+42")
+        assert bad_result_four == {:error, :eshortline}
+    end
+
+    test "parses data from PTIS Location entry" do
+        {:ok, result} = Parser.parse_msg_body("Location[Operator:910, Workpiece:802, Pattern:509, GPS:>RPV54109+4224023-0711289000018812<]")
+        assert result ==
+            %{
+                type: "Location",
+                operator: "910",
+                workpiece: "802",
+                pattern: "509",
+                gps: %{
+                    time: "54109",
+                    lat: "+4224023",
+                    long: "-07112890",
+                    speed: "000",
+                    heading: "188",
+                    source: "1",
+                    age: "2"
+                }
+            }
+        {:ok, result} = Parser.parse_msg_body("SAM[Operator:910, Workpiece:742, Pattern:712, ID:210, Type:1, Seq:1501833210, GPS:>RPV54105+4208039-0714003802604812<]")
+        assert result == %{type: "SAM"}
+    end
+
+    test "parses TAIP GPS data" do
+        {:ok, result} = Parser.parse_gps(">RPV54109+4224023-0711289000018812<")
+        assert result == %{
+            time: "54109",
+            lat: "+4224023",
+            long: "-07112890",
+            speed: "000",
+            heading: "188",
+            source: "1",
+            age: "2"
+        }
+    end
+
+    test "parses PTIS file" do
+        assert Parser.parse_file("data/test-AVL.txt") == [
+            %{timestamp: ~N[2017-08-04 01:01:49], vehicle_id: "1716", type: "Location", operator: "910", workpiece: "0", pattern: "9999",
+                gps: %{time: "54105", lat: "+4233913", long: "-07106041", speed: "006", heading: "318", source: "1", age: "1"}},
+            %{timestamp: ~N[2017-08-04 11:01:49], vehicle_id: "1822", type: "SAM Ack"},
+            %{timestamp: ~N[2017-08-04 11:01:48], vehicle_id: "1651", type: "Initialize Pattern Ack"},
+            %{timestamp: ~N[2017-08-04 11:01:48], vehicle_id: "1625", type: "Location", operator: "0", workpiece: "0", pattern: "0",
+                gps: %{time: "54106", lat: "+4237434", long: "-07107818", speed: "000", heading: "280", source: "1", age: "2"}}
+        ]
+    end
 end
