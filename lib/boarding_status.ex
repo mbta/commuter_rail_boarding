0379992fcb777f2d704f3c8caebe409262932e43
Paul Swartz <paulswartz@gmail.com>
2017-11-06 12:21:30 -0500

    additional logging of parse warnings

--- lib/boarding_status.ex
+++ lib/boarding_status.ex
@@ -85,8 +85,8 @@ defmodule BoardingStatus do
        }
       }
     else
-      _ ->
-        _ = Logger.warn(fn -> "unable to parse firebase map: #{inspect map}" end)
+      error ->
+        _ = Logger.warn(fn -> "unable to parse firebase map: #{inspect map}: #{inspect error}" end)
         :error
     end
   end
@@ -111,18 +111,24 @@ defmodule BoardingStatus do
     end
   end
 
-  defp create_trip_id(_route_id, "", keolis_trip_id) do
+  defp create_trip_id(route_id, "", keolis_trip_id) do
     # no trip name, build a new trip_id
+    _ = Logger.warn(fn ->
+      "creating trip for Keolis trip #{route_id} (#{keolis_trip_id})"
+    end)
     {:ok, "CRB_" <> keolis_trip_id, :unknown, true}
   end
   defp create_trip_id(route_id, trip_name, keolis_trip_id) do
     case TripCache.route_trip_name_to_id(route_id, trip_name) do
       {:ok, trip_id, direction_id} ->
+        _ = Logger.warn(fn ->
+          "matched trip #{trip_id} based on #{route_id} #{trip_name}"
+        end)
         {:ok, trip_id, direction_id, false}
       :error ->
         # couldn't match the trip name: log a warning but build a trip ID
         # anyways.
-        Logger.warn(fn ->
+        _ = Logger.warn(fn ->
           "unexpected missing GTFS trip ID: \
 route #{route_id}, name #{trip_name}, trip ID #{keolis_trip_id}"
         end)
