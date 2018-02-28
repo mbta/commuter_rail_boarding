45e18d99f4624221f78c7848401d406db4e941b8
Paul Swartz <paul@paulswartz.net>
2018-02-28 07:55:16 -0500

    update to Elixir 1.6.2

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule CommuterRailBoarding.Mixfile do
     [
       app: :commuter_rail_boarding,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.6",
       start_permanent: Mix.env == :prod,
       elixirc_paths: elixirc_paths(Mix.env),
       deps: deps(),
