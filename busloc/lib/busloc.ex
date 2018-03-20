464c838645c36594f450067f56f57db2cd02b1fd
Paul Swartz <paul@paulswartz.net>
2018-03-20 16:50:48 -0400

    initial busloc commit

---
+++ busloc/lib/busloc.ex
@@ -0,0 +1,18 @@
+defmodule Busloc do
+  @moduledoc """
+  Documentation for Busloc.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Busloc.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ busloc/mix.exs
@@ -0,0 +1,32 @@
+defmodule Busloc.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :busloc,
+      version: "0.1.0",
+      elixir: "~> 1.6",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ex_aws, "~> 2.0"},
+      {:ex_aws_s3, "~> 2.0"},
+      {:httpoison, "~> 1.0"},
+      {:sweet_xml, "~> 0.6"},
+      {:timex, "~> 3.2"},
+      {:xml_builder, "~> 0.1"}
+    ]
+  end
+end
