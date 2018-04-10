d4821a31b999d5d663407f6d1baae57303565f44
Chris Hayes <cwhayes711@gmail.com>
2018-04-10 13:39:50 -0400

    Add test for JSON validation logging so Codecov lets me merge

--- train_loc/test/trainloc/manager_test.exs
+++ train_loc/test/trainloc/manager_test.exs
@@ -107,4 +107,22 @@ defmodule TrainLoc.ManagerTest do
       refute capture_log(fun) =~ PreviousBatch.only_old_locations_warning()
     end
   end
+
+  describe "vehicles_from_data/1" do
+    @invalid_json %{
+      "Latitude" => 42.37405,
+      "Longitude" => -71.07496,
+      "TripID" => 0,
+      "Speed" => 0,
+      "Update Time" => "2018-01-16T15:03:27Z",
+      "VehicleID" => 1633,
+      "WorkID" => 0
+    }
+    test "logs message if vehicle validation fails" do
+      fun = fn ->
+        Manager.vehicles_from_data(@invalid_json)
+      end
+      assert capture_log(fun) =~ "Manager Vehicle Validation"
+    end
+  end
 end
