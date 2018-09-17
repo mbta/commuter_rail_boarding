62efe4cb73cac1f3bf8bce4b98c987db1fbac1a9
Paul Swartz <paul@paulswartz.net>
2018-09-17 10:52:44 -0400

    log more granular timings

--- apps/busloc/lib/busloc/fetcher/samsara_fetcher.ex
+++ apps/busloc/lib/busloc/fetcher/samsara_fetcher.ex
@@ -28,24 +28,34 @@ defmodule Busloc.Fetcher.SamsaraFetcher do
       now = DateTime.utc_now()
 
       url
-      |> HTTPoison.post!(config(SamsaraFetcher, :post_body))
-      |> Map.get(:body)
-      |> Jason.decode!()
-      |> Map.get("vehicles")
-      |> Enum.reject(&(Map.get(&1, "time", 0) == 0))
-      |> Enum.map(&Vehicle.from_samsara_json/1)
-      |> Enum.map(&log_vehicle(&1, now))
-      |> Enum.each(&Busloc.State.update(:transitmaster_state, &1))
+      |> time("fetch", &HTTPoison.post!(&1, config(SamsaraFetcher, :post_body)))
+      |> time("map_get_body", &Map.get(&1, :body))
+      |> time("jason", &Jason.decode!/1)
+      |> time("map_get_vehicles", &Map.get(&1, "vehicles"))
+      |> time("reject", fn vs -> Enum.reject(vs, &(Map.get(&1, "time", 0) == 0)) end)
+      |> time("from_json", fn vs -> Enum.map(vs, &Vehicle.from_samsara_json/1) end)
+      |> time("log", fn vs -> Enum.map(vs, &log_vehicle(&1, now)) end)
+      |> time("update", fn vs -> Enum.each(vs, &Busloc.State.update(:transitmaster_state, &1)) end)
     end
 
     {time, _} = :timer.tc(process)
 
     Logger.debug(fn ->
-      "#{__MODULE__}.process took ms=#{time / 1_000}"
+      "#{__MODULE__} timing name=process ms=#{time / 1_000}"
     end)
 
     Process.send_after(self(), :timeout, config(SamsaraFetcher, :fetch_rate))
     {:noreply, state}
+  end
+
+  def time(arg, log_name, fun) do
+    {time, ret} = :timer.tc(fun, [arg])
+
+    Logger.debug(fn ->
+      "#{__MODULE__} timing name=#{log_name} ms=#{time / 1_000}"
+    end)
+
+    ret
   end
 
   defp log_vehicle(vehicle, now) do
