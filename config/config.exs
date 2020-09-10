812f0f7f67a6cdc8738ceee91a71a7652f1c8ce5
Frank LaNasa <fjlanasa@Franks-MacBook-Pro.local>
2020-09-10 15:15:55 -0400

    chore: update config to substitute new stop_ids

--- config/config.exs
+++ config/config.exs
@@ -3,8 +3,9 @@ use Mix.Config
 config :commuter_rail_boarding,
   firebase_url: {:system, "CRB_FIREBASE_URL"},
   stop_ids: %{
-    "Boston" => "South Station",
-    "Boston North Station" => "North Station"
+    "South Station" => "NEC-2287",
+    "North Station" => "BNT-0000",
+    "Back Bay" => "NEC-2276"
   },
   headsigns: %{
     "Forge Park/495" => "Forge Park / 495",
