bc814a82b70bc7c320488359e63d7bcd30d3af23
Paul Swartz <paul@paulswartz.net>
2020-11-30 10:33:09 -0500

    chore: update CRB tests for Fall CR rating

--- apps/commuter_rail_boarding/lib/schedule_cache.ex
+++ apps/commuter_rail_boarding/lib/schedule_cache.ex
@@ -15,10 +15,10 @@ defmodule ScheduleCache do
   @doc """
   Returns the stop_sequence for a trip/stop ID pair.
 
-  iex> ScheduleCache.stop_sequence("CR-Weekday-Summer-20-820", "Back Bay")
+  iex> ScheduleCache.stop_sequence("CR-Weekday-Fall-20-820", "Back Bay")
   {:ok, 9}
 
-  iex> ScheduleCache.stop_sequence("CR-Weekday-Summer-20-813", "South Station")
+  iex> ScheduleCache.stop_sequence("CR-Weekday-Fall-20-813", "South Station")
   {:ok, 1}
 
   iex> ScheduleCache.stop_sequence("unknown", "Back Bay")
