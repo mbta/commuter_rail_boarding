735b16e436b1b5dba0b18f6230739159da4f55a8
Beatrix Klebe <bklebe@mbta.com>
2022-01-25 15:38:06 -0500

    fix: disable SASL logging
    SASL logs are producing a lot of noise on Erlang 24/Elixir 1.13

--- config/prod.exs
+++ config/prod.exs
@@ -7,18 +7,12 @@ config :commuter_rail_boarding, Uploader.S3,
   requestor: ExAws,
   bucket: {:system, "S3_BUCKET"}
 
-# Configures Elixir's Logger
-config :sasl, errlog_type: :error
-
 config :logger,
   truncate: :infinity,
-  handle_sasl_reports: true,
   level: :debug,
   backends: [:console]
 
 config :ehmon, :report_mf, {:ehmon, :info_report}
-
-config :sasl, errlog_type: :error
 
 config :train_loc,
   s3_api: TrainLoc.S3.HTTPClient,
